640f15689de22476da7da21d7ed90c4389b8cc5e
hituzi no sippo <dev@hituzi-no-sippo.me>
2022-07-11 16:51:21 +0900

    chore(git-commit-message): add setup shell for lint git commit message
    Using Cocogitto.
    
    References
    
    - https://docs.cocogitto.io/

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -55,6 +55,11 @@ function __setup_github_actions_linter() {
 
 	go install github.com/rhysd/actionlint/cmd/actionlint@latest
 }
+function __setup_git_commit_message() {
+	info "Setup git commit message linter"
+
+	cargo install cocogitto
+}
 function __setup_git_hooks() {
 	info "Setup git hooks manager"
 
@@ -80,6 +85,7 @@ function setup() {
 	__setup_yaml_tools
 	__setup_toml_tools
 	__setup_github_actions_linter
+	__setup_git_commit_message
 	__setup_git_hooks
 	__setup_natural_language
 
