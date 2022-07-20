f1a5c0c586c41c7aa635388c30012e0c45971098
hituzi no sippo <dev@hituzi-no-sippo.me>
2022-07-20 15:23:40 +0900

    style(JSON): add dprint of JSON formatter
    References
    
    - https://dprint.dev/

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,8 +9,20 @@ function info() {
 	printf "\n%s\n" "${1}"
 }
 
+function __setup_json_tools() {
+	info "Setup JSON tools"
+
+	function __setup_formatter() {
+		cargo install dprint
+	}
+
+	__setup_formatter
+}
+
 function setup() {
 	info "Setup Start"
+
+	__setup_json_tools
 
 	info "Setup Complete"
 }
